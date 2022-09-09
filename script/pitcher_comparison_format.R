a1d7c14547ed0bb45b51cf8d0cbdd3275058357b
DESKTOP-P2K2H5B\easyu <ot.shunie.rt@gmail.com>
2022-09-09 20:12:41 +0900

    ブレイクチャート 比較サンプル

--- script/pitcher_comparison_format.R
+++ script/pitcher_comparison_format.R
@@ -3,6 +3,7 @@ p_load(baseballr)
 p_load(tidyverse)
 p_load(lubridate)
 p_load(patchwork)
+p_load(viridis)
 source("functions/make_breakchart.R")
 source("functions/make_okuyukichart.R")
 source("functions/make_pitchLocation.R")
@@ -12,21 +13,23 @@ source("cheatsheets/event_files.R")
 
 # player name
 
-LN <- 'Darvish'
-FN <- 'Yu'
+LN <- c('Ohtani', 'Darvish')
+FN <- c('Shohei', 'Yu')
 
-#playerid_lookup(LN, FN) %>%
-#  select(first_name, last_name, birth_year, mlbam_id)
+playerid_lookup(LN[1], FN[1]) %>%
+  select(first_name, last_name, birth_year, mlbam_id)
+playerid_lookup(LN[2], FN[2]) %>%
+  select(first_name, last_name, birth_year, mlbam_id)
 
-id <- 506433
-yr <- 2022
+id <- c(660271, 506433)
+yr <- c(2022, 2022)
 
-player <- map_df(
-  .x = yr,
+player <- map2_df(
+  .x = id, .y = yr,
   .f = ~ scrape_statcast_savant(
-    start_date = paste0(.x, '-', 3, '-', '1'),
-    end_date = paste0(.x, '-', 11, '-', '30'),
-    player_type = "pitcher", playerid = id
+    start_date = paste0(.y, '-', 3, '-', '1'),
+    end_date = paste0(.y, '-', 11, '-', '30'),
+    player_type = "pitcher", playerid = .x
   ) %>%
     filter(!(pitch_type %in% c("PO", "IN", "null", ""))) %>%
     dplyr::filter(game_type != 'S') %>%
@@ -38,27 +41,47 @@ player <- map_df(
       release_speed_km = release_speed * 1.61,
     )
 )
-  
 
-p1 <- make_breakchart(
-  player, "pfx_x_cm", "pfx_z_cm", 
-  colour_palette = pitch_colour,
-  plot_type = "colour"#,
-  #split = "game_year"
-) +
-  ggtitle(label = paste0(FN, " ", LN, " Break Chart"))
+p1 <- ggplot2::ggplot(player) +
+  ggplot2::aes(x = pfx_x_cm, 
+               y = pfx_z_cm, 
+               colour = player_name) +
+  ggplot2::geom_point(#shape = "circle filled", colour = "black", 
+                      alpha = .4) +
+  ggplot2::theme_bw() +
+  ggplot2::xlab("Horizontal Break (cm)") + ggplot2::ylab("Induced Vertical Break (cm)") +
+  ggplot2::geom_hline(yintercept = 0) + ggplot2::geom_vline(xintercept = 0) +
+  scale_colour_manual(values = viridis(2, begin = .7, end = 0, option = "B")) +
+  ggplot2::labs(title = paste0(FN[1], " ", LN[1], " vs. ", FN[2], " ", LN[2], ": Pitch Overlay"), colour = "Pitcher")
+
 p1
-ggsave(paste0("fig/", LN, FN, "_breakchart.png"), p1, width = 6, height = 5)
+
+ggsave(paste0("fig/", LN[[1]], "_", LN[[2]], yr[1], "_overlay.png"), p1, width = 6, height = 5)
+
+p1 <- ggplot2::ggplot(player %>% filter(pitch_type == "FF")) +
+  ggplot2::aes(x = pfx_x_cm, 
+               y = pfx_z_cm, 
+               colour = player_name) +
+  ggplot2::geom_point(#shape = "circle filled", colour = "black", 
+    alpha = .4) +
+  ggplot2::theme_bw() +
+  ggplot2::xlab("Horizontal Break (cm)") + ggplot2::ylab("Induced Vertical Break (cm)") +
+  ggplot2::geom_hline(yintercept = 0) + ggplot2::geom_vline(xintercept = 0) +
+  scale_colour_manual(values = viridis(2, begin = .7, end = 0, option = "B")) +
+  ggplot2::labs(title = paste0(FN[1], " ", LN[1], " vs. ", FN[2], " ", LN[2], ": Pitch Overlay \n4-Seam Fastball"), colour = "Pitcher")
+
+p1
+
+ggsave(paste0("fig/", LN[[1]], "_", LN[[2]], yr[1], "_overlay_4Seam.png"), p1, width = 6, height = 5)
 
 p2 <- make_okuyukichart(
   player, "release_speed_km", "pfx_z_cm",
   colour_palette = pitch_colour,
-  plot_type = "colour",
-  split = "game_year"
+  plot_type = "colour"
 ) +
-  ggtitle(label = paste0(FN, " ", LN, "\nVelocity-Vertical Break Chart"))
-p2
-ggsave(paste0("fig/", LN, FN, "_okuyukichart.png"), p2, width = 6, height = 5)
+  ggtitle(label = paste0(FN, " ", LN, "\nVelocity-Vertical Break Chart: ", yr))
+
+ggsave(paste0("fig/", LN, FN, yr, "_okuyukichart.png"), p2, width = 6, height = 5)
 
 left <- make_pitchLocation(
   player %>% filter(stand == "L"),
@@ -88,9 +111,9 @@ g1 <- left / right
 
 ggsave(paste0("fig/", LN, FN, yr, "_location.png"), g1, width = 4, height = 6)
 
-label <- c("Season", "Pitch Type", "#", "Usage", "Velocity (km)", "Spin Rate (rpm)", "HorzBreak (cm)", "InducedVertBreak (cm)", "Zone%", "CStr%", "Swing%", "Contact%", "SwStr%", "CSW%", "GB%", "LD%", "Hard%", "xwOBAcon", "wOBA", "PitchValue", "PV/C")
+label <- c("Pitch Type", "#", "Usage", "Velocity (km)", "Spin Rate (rpm)", "HorzBreak (cm)", "InducedVertBreak (cm)", "Zone%", "CStr%", "Swing%", "Contact%", "SwStr%", "CSW%", "GB%", "LD%", "Hard%", "xwOBAcon", "wOBA", "PitchValue", "PV/C")
 player %>%
-  dplyr::group_by(game_year, pitch_name) %>%
+  dplyr::group_by(pitch_name) %>%
   dplyr::summarise(number = n(), usage = round(100 * n() / nrow(player), 1),
                    speed = round(mean(release_speed_km), 1),
                    srate = round(mean(release_spin_rate, na.rm = T), 1),
@@ -114,7 +137,7 @@ player %>%
                    PV = round(sum(-delta_run_exp, na.rm = T), 2),
                    PV_C = round(sum(-delta_run_exp, na.rm = T) / n() * 100, 2),
   ) %>%
-  dplyr::arrange(pitch_name) %>%
+  dplyr::arrange(desc(number)) %>%
   set_names(label) -> summary
 
 write_excel_csv(summary, file = paste0("table/", LN, FN, yr, "_summary.csv"))
